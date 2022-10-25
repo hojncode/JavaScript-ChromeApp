14d6b637d7b466917e6d3a227f1ebe042b9f1eca
hojncode <glicm12@gmail.com>
2022-10-25 15:54:54 +0900

    [study] weatherOpenApi 활용 - fetch().then 사용

--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,21 @@
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+const API_KEY = "";
+
 function onGeoOk(position) {
     // console.log(position);
     const latitude = position.coords.latitude;
-    const longitude = position.coords.latitude;
-    console.log("your location ", latitude, longitude)
+    const longitude = position.coords.longitude;
+    // console.log("your location ", latitude, longitude);
+    const url =  `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    fetch(url).then(res => res.json()).then(data => {
+        const weather = document.querySelector("#weather span:first-child")
+        const city = document.querySelector("#weather span:last-child")
+        const temp = document.querySelector("#weather span:last-child")
+        temp.innerText = 
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}℃`;
+        })
 }
 
 function onGeoError() {
